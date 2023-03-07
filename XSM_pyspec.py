f9a49bd0fe3fbf903e6abba8b6d0d1848c404837
Asif Mohamed Mandayapuram <asifmp97@gmai.com>
2023-03-07 12:54:33 +0530

    Major changes to fitting

--- XSM_pyspec.py
+++ XSM_pyspec.py
@@ -15,25 +15,21 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import xspec as xp
 
-from data_utils import create_daxss_pha, read_daxss_data
 from fit_utils import chisoth_2T, do_grppha
+from plot_fits import plot_individual
 
 plt.style.use("fivethirtyeight")
-flare_num = 7
-flare_dir = f"./data/pha/flare_num_{flare_num}/xsm"
+
 observation_file = "./flare_filtering/data/flare_observation.h5"
 
-observation_table = pd.read_hdf(observation_file, "obs")
-
 xsm_origin_time = datetime(2017, 1, 1)
-
 utc2met = lambda time: (time - xsm_origin_time).total_seconds()
 
 
-#%% Chosse flare
 def create_pha_files(flare_num, bin_size):
+    flare_dir = f"./data/pha/flare_num_{flare_num}/xsm"
+    observation_table = pd.read_hdf(observation_file, "obs")
     flare = observation_table.loc[flare_num]
     time_beg = flare["event_starttime"]
     time_end = flare["event_endtime"]
@@ -68,41 +64,72 @@ def create_pha_files(flare_num, bin_size):
 
 
 #%% Create files
-CREATE = False
-BIN = False
-# Create initial PHA files
-if not os.path.isdir(f"{flare_dir}/orig_pha/"):
-    CREATE = True
-if CREATE:
-    shutil.rmtree(f"{flare_dir}/orig_pha/", ignore_errors=True)
-    resamp = create_pha_files(flare_num, bin_size="27S")
+def fit_xsm(
+    flare_num,
+    FIP_elements=["Mg", "Si", "S", "Ar", "Ca", "Fe"],
+    CREATE=False,
+    BIN=False,
+    cutoff_cps=1,
+    min_E=1.3,
+    max_E=10.0,
+):
+    """TODO: Docstring for fit_xsm.
 
-# Run grppha on files to group
-orig_PHA_file_list = glob.glob(f"{flare_dir}/orig_pha/*.pha")
-orig_PHA_file_list.sort()
-PHA_file_list = [
-    orig_i.replace("/orig_pha/", "/grpd_pha/") for orig_i in orig_PHA_file_list
-]
+    Parameters
+    ----------
+    flare_num : int, index of flare in the list.
+    FIP_elements : list of elements to be considered as FIP.
+    CREATE : bool, Whether to create_pha_files.
+    BIN : bool, Whetther to bin the pha files using grbpha
+    cutoff_cps: float, threshold cps to bin i.e the grppha
+    min_E : float, minimum energy of spectra to start fit from
+    max_E : float, optional, maximum energy to fit from
 
-cutoff_cps = 1
-if not os.path.isdir(f"{flare_dir}/grpd_pha/"):
-    BIN = True
-if BIN:
-    shutil.rmtree(f"{flare_dir}/grpd_pha/", ignore_errors=True)
-    os.makedirs(f"{flare_dir}/grpd_pha")
-    do_grppha(orig_PHA_file_list, PHA_file_list, cutoff_cps)
-    os.system(f"cp {flare_dir}/orig_pha/*.arf {flare_dir}/grpd_pha/")
+    Returns
+    -------
+    Datframe with results
 
-# #%% Initialise
+    """
+    # Create initial PHA files
+    flare_dir = f"./data/pha/flare_num_{flare_num}/xsm"
+    if not os.path.isdir(f"{flare_dir}/orig_pha/"):
+        CREATE = True
+    if CREATE:
+        shutil.rmtree(f"{flare_dir}/orig_pha/", ignore_errors=True)
+        resamp = create_pha_files(flare_num, bin_size="27S")
 
-chiso = chisoth_2T(PHA_file_list, flare_dir)
-# FIP_elements = ["Mg", "Si", "S", "Ar", "Ca", "Fe"]
+    # Run grppha on files to group
+    orig_PHA_file_list = glob.glob(f"{flare_dir}/orig_pha/*.pha")
+    orig_PHA_file_list.sort()
+    PHA_file_list = [
+        orig_i.replace("/orig_pha/", "/grpd_pha/") for orig_i in orig_PHA_file_list
+    ]
+
+    if not os.path.isdir(f"{flare_dir}/grpd_pha/"):
+        BIN = True
+    if BIN:
+        shutil.rmtree(f"{flare_dir}/grpd_pha/", ignore_errors=True)
+        os.makedirs(f"{flare_dir}/grpd_pha")
+        do_grppha(orig_PHA_file_list, PHA_file_list, cutoff_cps)
+        os.system(f"cp {flare_dir}/orig_pha/*.arf {flare_dir}/grpd_pha/")
+
+    # #%% Initialise
+
+    chiso = chisoth_2T(PHA_file_list, flare_dir)
+    chiso.init_chisoth(FIP_elements, error_sigma=4.00)
+
+    #%%Fit
+    chiso.arf_files_list = [
+        orig_i.removesuffix(".pha") + (".arf") for orig_i in orig_PHA_file_list
+    ]
+    df = chiso.fit(min_E, max_E)
+    return df
+
+
+flare_num = 23
+
 FIP_elements = ["Mg", "Si", "S", "Ar", "Fe"]
-chiso.init_chisoth(FIP_elements)
+df = fit_xsm(flare_num=flare_num, FIP_elements=FIP_elements)
 
-#%%Fit
-chiso.arf_files_list = [
-    orig_i.removesuffix(".pha") + (".arf") for orig_i in orig_PHA_file_list
-]
-
-df = chiso.fit(min_E=1.3)
+#%% plot
+plot_individual(instrument="xsm", flare_num=flare_num)

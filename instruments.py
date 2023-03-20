9a3a0fb46370c22719471df5ae29b880cd6f3b2c
Asif Mohamed Mandayapuram <asifmp97@gmai.com>
2023-03-20 12:37:10 +0530

    fixup! Moved grppha into instruments

--- instruments.py
+++ instruments.py
@@ -9,6 +9,24 @@ from data_utils import create_daxss_pha,read_daxss_data
 import shutil
 import os
 
+def do_grppha(
+    file_list,
+    out_put_file_list,
+    threshold_counts,
+):
+    """
+    The function will do gpb pha on files based on cutoff cps.
+
+    Parameters
+    ----------
+    file_list : list of files
+    cutoff_cps : cutoff cps
+    out_put_file_list : list of output file names.
+    """
+    for in_file, out_file in zip(file_list, out_put_file_list):
+        command = f"grppha infile='{in_file}' outfile='!{out_file}' comm='GROUP MIN {threshold_counts}&exit' "
+        os.system(command)
+
 class instrument:
     """Parent class for instruents"""
     def __init__(self,output_dir=None,PHA_file_list=None,arf_file_list=None,):
@@ -24,24 +42,6 @@ class instrument:
         self.name = "instrument"
         self.set_output_dir(output_dir)
         self.set_pha_files(PHA_file_list, arf_file_list)
-
-    def do_grppha(
-        file_list,
-        out_put_file_list,
-        threshold_counts,
-    ):
-        """
-        The function will do gpb pha on files based on cutoff cps.
-
-        Parameters
-        ----------
-        file_list : list of files
-        cutoff_cps : cutoff cps
-        out_put_file_list : list of output file names.
-        """
-        for in_file, out_file in zip(file_list, out_put_file_list):
-            command = f"grppha infile='{in_file}' outfile='!{out_file}' comm='GROUP MIN {threshold_counts}&exit' "
-            os.system(command)
 
     def set_output_dir(self, output_dir):
         """
@@ -109,7 +109,7 @@ class instrument:
             pha_file_i.replace("/orig_pha/", "/grpd_pha/") for pha_file_i in self.PHA_file_list
         ]
         os.makedirs(f"{self.output_dir}/grpd_pha")
-        self.do_grppha(out_PHA_file_list, PHA_file_list, min_count)
+        do_grppha(out_PHA_file_list, PHA_file_list, min_count)
         self.PHA_file_list = out_PHA_file_list
         return out_PHA_file_list
 
